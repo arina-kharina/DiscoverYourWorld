4b3b8cbb8569a12f67c22d6fcdf40fd8fc8c123f
arina-kharina <arina.v.harina@gmail.com>
2024-01-08 13:59:33 +0400

    first

---
+++ README.md
@@ -0,0 +1,2 @@
+"# DiscoverYourWorld" 
+"# DiscoverYourWorld" 
---
+++ create.bat
@@ -0,0 +1,7 @@
+echo "# DiscoverYourWorld" >> README.md
+git init
+git add *
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/arina-kharina/DiscoverYourWorld.git
+git push -u origin main
